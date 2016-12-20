92f80a4a42a9fce6d99f83c3fbaff5fb52e6fa4f
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-12-20 19:04:30 +0900

    define create socket name function

--- src/key_generator.cpp
+++ src/key_generator.cpp
@@ -6,12 +6,20 @@ KeyGenerator::KeyGenerator() {
 KeyGenerator::~KeyGenerator() {
 }
 
-// shmKeyname: hoge, key: 1 -> Generate Key: hoge01
+// name: hoge, key: 1 -> Generate Key: hoge01
+//std::string
+//KeyGenerator::createSocketName(std::string baseKey, unsigned int key) {
+//    std::ostringstream sout;
+//    sout << std::setfill('0') << std::setw(2) << key;
+//    std::string appShmKey = baseKey + sout.str();
+//    return appShmKey;
+//}
+
+
 std::string
-KeyGenerator::createKey(char* baseKey, unsigned int key) {
-    std::ostringstream sout;
-    sout << std::setfill('0') << std::setw(2) << key;
-    std::string appShmKey = baseKey + sout.str();
-    return appShmKey;
+KeyGenerator::createSocketName(std::string baseKey, unsigned int key) {
+    std::string socketName = baseKey + std::to_string(key);
+    return socketName;
 }
+
 
