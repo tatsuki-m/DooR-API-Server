b629d40dd54d75d225ab5b716fb19522723d82f2
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-12-21 09:51:10 +0900

    delete needless function

--- src/key_generator.cpp
+++ src/key_generator.cpp
@@ -6,15 +6,6 @@ KeyGenerator::KeyGenerator() {
 KeyGenerator::~KeyGenerator() {
 }
 
-// name: hoge, key: 1 -> Generate Key: hoge01
-//std::string
-//KeyGenerator::createSocketName(std::string baseKey, unsigned int key) {
-//    std::ostringstream sout;
-//    sout << std::setfill('0') << std::setw(2) << key;
-//    std::string appShmKey = baseKey + sout.str();
-//    return appShmKey;
-//}
-//
 std::string
 KeyGenerator::createDoorShmKey(std::string baseKey, unsigned int workerID, unsigned int coutner) {
     std::ostringStream sout1, sout2;
@@ -24,12 +15,9 @@ KeyGenerator::createDoorShmKey(std::string baseKey, unsigned int workerID, unsig
     return doorShmKey;
 }
 
-
-
 std::string
 KeyGenerator::createSocketName(std::string baseKey, unsigned int key) {
     std::string socketName = baseKey + std::to_string(key);
     return socketName;
 }
-
 
