8383c9b6e2504d9fc59c80f52b3f98278b3dcccf
tatsuki-m <tatsukitatsuki0923@gmail.com>
2016-12-28 16:04:51 +0900

    change return value

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,15 @@
 #include "door_api_manager.h"
 #include "unix_domain_socket_server.h"
 
-/*
-#include "door_shared_memory/shared_key.h"
 #include "door_shared_memory/shared_memory.h"
 #include "door_shared_memory/shared_packet_information.h"
 #include "door_shared_memory/dpi.h"
-*/
 
 
 int
 main() {
     // check main thread 
     std::cout << "main" << std::this_thread::get_id() << std::endl;
-
 
     // initialize socket & server instance
     UnixDomainSocketServer socket = UnixDomainSocketServer();
@@ -47,25 +43,30 @@ main() {
     //delete[] sharedData;
     //delete sharedKeyMemory1;
 */
-
 /*
     std::string key = "hoge";
     //char data[1000] = "hoge";
     //char ipdata[10] = "hoge";
     //Dpi* dpi = new Dpi(1, ipdata, ipdata, 2, 3, data);
-    unsigned int id = 1;
-    unsigned int srcPort = 1;
-    unsigned int dstPort = 1;
-    Dpi* dpi = new Dpi(id,srcPort, dstPort);
-    std::cout << sizeof(Dpi) << std::endl;
-    // std::cout << sizeof(*dpi) << std::endl;
+    //
+/*
     SharedMemory<Dpi, SharedPacketInformation>* sharedKeyMemory2 = new SharedMemory<Dpi, SharedPacketInformation>(key);
-    // sharedKeyMemory2->write(dpi);
     Dpi* dpi2=NULL;
     sharedKeyMemory2->read(&dpi2);
     std::cout << dpi2->dstPort_ << std::endl;
-    delete dpi;
     delete dpi2;
+*/
+
+/*
+    unsigned int id = 1;
+    unsigned int srcPort = 1;
+    unsigned int dstPort = 1;
+    Dpi* dpi = new Dpi(id, srcPort, dstPort);
+    std::cout << sizeof(Dpi) << std::endl;
+    // std::cout << sizeof(*dpi) << std::endl;
+    SharedMemory<Dpi, SharedPacketInformation>* sharedKeyMemory2 = new SharedMemory<Dpi, SharedPacketInformation>(key);
+    sharedKeyMemory2->write(dpi);
+    delete dpi;
 */
 
     return 0;
--- src/shared_memory/src/shared_memory.cpp
+++ src/shared_memory/src/shared_memory.cpp
@@ -2,74 +2,76 @@
 
 template <class T, class U>
 SharedMemory<T, U>::SharedMemory(std::string sharedMemoryName) {
-    m_sharedMemoryName_ = sharedMemoryName.c_str();
+    sharedMemoryName_ = sharedMemoryName.c_str();
 }
 
 template <class T, class U>
 SharedMemory<T, U>::~SharedMemory() {
     // TODO: check shared memory is already deleted 
-    delete m_sharedMemoryName_;
 }
 
 template <class T, class U>
 bool
 SharedMemory<T, U>::write(T* sharedData) {
+/*
     std::cout << "SharedMemory::write()" << std::endl;
-    shared_memory_object shm(open_or_create, m_sharedMemoryName_, read_write);
+    shared_memory_object shm(open_or_create, sharedMemoryName_.c_str(), read_write);
     mapped_region region(shm, read_write);
     void *addr = region.get_address();
     m_sharedMemoryBuffer_ = static_cast<U*>(addr);
-    /*
+*/
     std::cout << "SharedMemory::write()" << std::endl;
-    shared_memory_object shm(open_or_create, m_sharedMemoryName_, read_write);
+    shared_memory_object shm(open_or_create, sharedMemoryName_.c_str(), read_write);
     shm.truncate(sizeof(U)+U::getSharedDataSize());
     mapped_region region(shm, read_write);
     void *addr = region.get_address();
     m_sharedMemoryBuffer_ = new (addr) U;
-    */
 
     try {
-     //   while(true) {
+        while(true) {
             m_sharedMemoryBuffer_->writer_.wait();
                 std::cout << "start writing" << std::endl;
                 m_sharedMemoryBuffer_->writeDataToShm(sharedData);
             m_sharedMemoryBuffer_->reader_.post();
-      //  };
+         }
     } catch (interprocess_exception& e) {
         std::cout << e.what() << std::endl;
+        return false;
    }
     return true;
 }
 
 template <class T, class U>
-void
+bool
 SharedMemory<T, U>::read(T** sharedData) {
+/*
     std::cout << "SharedMemory::read()" << std::endl;
-    shared_memory_object shm(open_or_create, m_sharedMemoryName_, read_write);
+    shared_memory_object shm(open_or_create, sharedMemoryName_.c_str(), read_write);
     shm.truncate(sizeof(U)+U::getSharedDataSize());
     mapped_region region(shm, read_write);
     void *addr = region.get_address();
     m_sharedMemoryBuffer_ = new (addr) U;
-    /*
+*/
     std::cout << "SharedMemory::read()" << std::endl;
-    shared_memory_object shm(open_or_create, m_sharedMemoryName_, read_write);
+    shared_memory_object shm(open_or_create, sharedMemoryName_.c_str(), read_write);
     mapped_region region(shm, read_write);
     void *addr = region.get_address();
     m_sharedMemoryBuffer_ = static_cast<U*>(addr);
-    */
 
     try {
-        while(true) {
+        //while(true) {
             m_sharedMemoryBuffer_->reader_.wait();
                 std::cout << "start reading" << std::endl;
                 *sharedData = new T;
                 m_sharedMemoryBuffer_->readDataFromShm(*sharedData);
-                std::cout << (*sharedData)->id_ << std::endl;
+                std::cout << "SharedMemory::read sharedData: " << (*sharedData)->id_ << std::endl;
             m_sharedMemoryBuffer_->writer_.post();
-        }
+       // }
     } catch (interprocess_exception& e) {
         std::cout << e.what() << std::endl;
+        return false;
     }
+    return true;
 }
 
 // Instantiation of explicit template
