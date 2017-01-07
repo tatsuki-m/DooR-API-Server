9c4cde24b175f6044b4e89a07420ad2360f97426
tatsuki-m <tatsukitatsuki0923@gmail.com>
2017-01-07 10:49:50 +0900

    change include file

--- src/door_api/src/door_api.h
+++ src/door_api/src/door_api.h
@@ -4,9 +4,8 @@
 #include <iostream>
 #include <string>
 
-#include "unix_domain_socket_client.h"
-
-#include "door_bridge/socket_ack.h"
+#include "door_unix_domain_socket_client/socket_ack.h"
+#include "door_unix_domain_socket_client/unix_domain_socket_client.h"
 #include "door_shared_memory/shared_memory.h"
 #include "door_shared_memory/shared_packet_information.h"
 #include "door_shared_memory/dpi.h"
