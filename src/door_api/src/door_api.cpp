e16f3a6a9e11eadbd4e1b66e31fc95fbf8ff44db
tatsuki-m <tatsukitatsuki0923@gmail.com>
2017-01-08 15:00:55 +0900

    delete needless function

--- src/door_api/src/door_api.cpp
+++ src/door_api/src/door_api.cpp
@@ -13,11 +13,5 @@ DoorApi::callDoorWithSem(std::string shmKey, std::string keyword) {
     SocketType type = ASK_DOOR;
     UnixDomainSocketClient socket = UnixDomainSocketClient(DOOR_BASE_SOCKET_NAME, type, shmKey);
     socket.run();
-
-/*
-    SharedMemory<Dpi, SharedPacketInformation>* doorShm = new SharedMemory<Dpi, SharedPacketInformation>(shmKey);
-    Dpi *dpi;
-    doorShm->read(&dpi);
-*/
 }
 
